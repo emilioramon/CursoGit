818293e4181f1e1fed74d4c81bffc8e51f7e7779
unknown <emilioramon@gmail.com>
2022-03-04 10:47:34 -0300

    Comienzo del proyecto

---
+++ conteo.py
@@ -0,0 +1,15 @@
+import collections
+import pprint
+
+nombre_archivo = 'pp.txt'
+
+
+with open(nombre_archivo,'r') as f:
+    conteo_caracteres = collections.Counter(f.read().upper())
+    salida = pprint.pformat(conteo_caracteres)
+
+#print(conteo_caracteres)
+
+
+print(salida)
+
