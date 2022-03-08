0184d451e651472cd4150ba06019008a4ef4109f
unknown <emilioramon@gmail.com>
2022-03-08 10:30:10 -0300

    Desde VSC 2

--- conteo.py
+++ conteo.py
@@ -5,15 +5,9 @@ nombre_archivo = 'pp.txt'
 
 #Comentario
 #Comentario 2
-<<<<<<< HEAD
-<<<<<<< HEAD
 #Rama main
-=======
 #Comentario branch
-=======
->>>>>>> 587e85831aad05cfedff2f5617b252aae1b0301d
-
->>>>>>> Rama1
+# Rama1
 
 with open(nombre_archivo,'r') as f:
     conteo_caracteres = collections.Counter(f.read().upper())
